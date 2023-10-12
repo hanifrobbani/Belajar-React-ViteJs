f6e12a46a907e4463b3c41504d11ee0b88b79ef1
hanifrobbani <mhanifrobbani51@gmail.com>
2023-10-12 16:52:41 +0700

    menambahkan library react vitejs

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function header(){
+  return(
+    <header>
+      <h1>Ini adalah header</h1>
+    </header>
+  );
+}
+
+function Main(){
+  return(
+    <header>
+      <h1>Ini adalah Main (content utama)</h1>
+    </header>
+  );
+}
+
+function footer(){
+  return(
+    <header>
+      <h1>Ini adalah footer</h1>
+    </header>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
